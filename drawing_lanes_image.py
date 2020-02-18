ad99a64970efc206a6df1ae89dfe10248294f8e8
Nihar <nihar1912@live.com>
2020-02-18 06:17:54 -0700

    drawing update

--- drawing_lanes_image.py
+++ drawing_lanes_image.py
@@ -11,14 +11,16 @@ image=mpimg.imread(filename)
 print("hello")
 #plt.imshow(image)
 
-#1. COnvert to greyscale
+#1. Convert to greyscale
 grey=ncv.Grey(image)
 #plt.imshow(grey)
+cv2.imshow('grey',grey)
 
 #2. smooth the image with a guassian blur
 kernel_size=13
 blur_grey=ncv.Gaussian(grey,kernel_size)
 #plt.imshow(blur_grey)
+cv2.imshow('blur_grey',blur_grey)
 
 #3. canny to do edge detection
 low=72
@@ -26,6 +28,7 @@ high=161
 #edges=cv2.Canny(blur_grey, low,high)
 edges=ncv.Canny(blur_grey,low,high)
 #plt.imshow(edges)
+cv2.imshow('edges',edges)
 
 #4. create a mask for a region of interest simultaneously
 mask=np.zeros_like(edges)
@@ -34,11 +37,12 @@ red=255
 roi=np.array([[(0,540),(400,300),(520,300),(960,540)]],dtype=np.int32)
 cv2.fillPoly(mask,roi,red)
 #plt.imshow(mask)
+cv2.imshow('mask',mask)
 
 #5. create an image which only has the road markings and the edge obtained from canny
 masked_edges=cv2.bitwise_and(edges,mask)
-plt.imshow(masked_edges)
-
+#plt.imshow(masked_edges)
+cv2.imshow('masked_edges',masked_edges)
 #6. do the hough transform on the masked_edges obtained
 rho=2
 theta=np.pi/180
@@ -51,6 +55,7 @@ for line in lines:
     for x1,y1,x2,y2 in line:
         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
 #plt.imshow(line_image)
+cv2.imshow('line_image',line_image)
 
 #7. improving line drawing
 xleft=[]
@@ -60,7 +65,7 @@ yright=[]
 
 coords=[(460,540),(460,300),(520,300),(960,540)]
 poly=Polygon(coords)
-yboth=np.linspace(540,350,num=2,endpoint=True)
+yboth=np.linspace(540,350,num=4,endpoint=True)
 
 for line in lines:
     for x1,y1,x2,y2 in line:
@@ -79,15 +84,16 @@ for line in lines:
 
 xleft=np.asarray(xleft)
 yleft=np.asarray(yleft)
-zleft=np.polyfit(xleft,yleft,1)
+zleft=np.polyfit(yleft,xleft,2)
+pleft=np.poly1d(zleft);
+xleft=pleft(yboth)
 
-xleft=((yboth-zleft[1])/zleft[0])
 
 xright=np.asarray(xright)
 yright=np.asarray(yright)
-zright=np.polyfit(xright,yright,1)
-
-xright=((yboth-zright[1])/zright[0])
+zright=np.polyfit(yright,xright,2)
+pright=np.poly1d(zright);
+xright=pright(yboth)
 
 yboth=yboth.astype(int)
 xleft=xleft.astype(int)
@@ -95,8 +101,25 @@ xright=xright.astype(int)
 
 final_image=np.copy(image)
 cv2.line(final_image,(xleft[0],yboth[0]),(xleft[1],yboth[1]),(255,0,0),5)
-cv2.line(final_image,(xright[0],yboth[0]),(xright[1],yboth[1]),(255,0,0),5)
-plt.imshow(final_image)
+cv2.line(final_image,(xleft[1],yboth[1]),(xleft[2],yboth[2]),(255,0,0),5)
+cv2.line(final_image,(xleft[2],yboth[2]),(xleft[3],yboth[3]),(255,0,0),5)
 
+cv2.line(final_image,(xright[0],yboth[0]),(xright[1],yboth[1]),(255,0,0),5)
+cv2.line(final_image,(xright[1],yboth[1]),(xright[2],yboth[2]),(255,0,0),5)
+cv2.line(final_image,(xright[2],yboth[2]),(xright[3],yboth[3]),(255,0,0),5)
+
+#plt.imshow(final_image)
+cv2.imshow('final_image',final_image)
+
+'''
+final_image_2=np.copy(image)
+npleft=np.array([xleft,yboth])
+npleft=npleft.reshape((-1,1,2))
+
+cv2.polylines(final_image_2,npleft,True,(255,0,0),thickness=5)
+plt.imshow(final_image_2)
+'''
+#cv2.show()
+cv2.waitKey(0)
 
 plt.show()
